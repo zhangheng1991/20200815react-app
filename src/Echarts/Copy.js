6b36ed75d8f838e46cac8ebe1ad63b486a3eb100
zhangheng1991 <404455037@qq.com>
2020-12-29 17:08:29 +0800

    zhangheng20201229

--- src/Echarts/Copy.js
+++ src/Echarts/Copy.js
@@ -6,35 +6,53 @@ class CopyCom extends React.Component {
   state = {
     title: '',
     textT: '没有选中！',
-    persent:66,
+    persent: 66,
   };
   onCopy = record => {
     console.log(record);
-    this.setState({ textT: record.address,persent:record.persent});
+    this.setState({ textT: record.address, persent: record.persent });
   };
-  onChangeNumber=(value)=>{
+  onChangeNumber = value => {
     //  console.log(value)
-     this.setState({ persent:value});
-  }
+    this.setState({ persent: value });
+  };
   render() {
-    const { textT,persent } = this.state;
+    const { textT, persent } = this.state;
+    const data = [];
     const dataSource = [
-      {
-        key: '1',
-        name: '胡彦斌',
+        // {
+        //   key: '1',
+        //   name: '胡彦斌',
+        //   age: 32,
+        //   address: '西湖区湖底公园1号',
+        //   persent: 50,
+        // },
+        // {
+        //   key: '2',
+        //   name: '胡彦祖',
+        //   age: 42,
+        //   address: '西湖区湖底公园2号',
+        //   persent: 90,
+        // },
+      ];
+  
+    for (let i = 1; i <= 100; i++) {
+      data.push({
+        key: i,
+        name: `Edward King ${i}`,
         age: 32,
-        address: '西湖区湖底公园1号',
-        persent:50
-      },
-      {
-        key: '2',
-        name: '胡彦祖',
-        age: 42,
-        address: '西湖区湖底公园2号',
-        persent:90
-      },
-    ];
+        address: `London, Park Lane no. ${i}`,
+      });
+      dataSource.push({
+        key:i,
+        name: '胡彦祖'+i,
+        age: i,
+        address: '西湖区湖底公园'+i+"号",
+        persent:i,
+      })
+    }
 
+   
     const columns = [
       {
         title: '姓名',
@@ -68,20 +86,26 @@ class CopyCom extends React.Component {
     };
     return (
       <div className={`${style['copyBox']}`}>
-        <InputNumber min={0} max={100} defaultValue={persent} onChange={this.onChangeNumber} />
+        <InputNumber
+          min={0}
+          max={100}
+          // defaultValue={persent}
+          value={persent}
+          onChange={this.onChangeNumber}
+        />
         <h2>当前百分比：{persent}</h2>
         <Progress
           strokeColor={{
             '0%': '#108ee9',
-            '20':"red",
-            '40':"green",
-            '60':"blue",
-            '80%':"yellow",
+            '20': 'red',
+            '40': 'green',
+            '60': 'blue',
+            '80%': 'yellow',
             '100%': '#87d068',
           }}
           percent={persent}
         />
-         <h2>当前选中复制：{textT}</h2>
+        <h2>当前选中复制：{textT}</h2>
         <Table {...dataT} />
       </div>
     );
