fc2c4949bba921d43bf222375333aec5256e6611
zhangheng1991 <404455037@qq.com>
2022-05-20 16:26:17 +0800

    zhangheng20220520 16:26tijiao

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -997,7 +997,7 @@ class Test extends React.Component {
                       // console.log(fiedLine,"fiedLine")
 
                       if (key + 1 === fiedLine) {
-                        return <div className={`${style["processBoxContent"]} ${style["processBoxContentF"]}`} style={{marginRight:index % 2 === 1?"17px":"0px"}}> <div className={style.file}>
+                        return <div className={`${style["processBoxContent"]} ${style["processBoxContentF"]}`} style={{marginRight:index % 2 === 1?"8px":"0px"}}> <div className={style.file}>
                           {itemD.title}-first
                         </div> <div><Icon type="plus-circle" /></div>  <Icon type="arrow-down" /> </div>
                       } if (index % 2 === 0) {
